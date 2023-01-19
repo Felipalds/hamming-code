958daf8b4ce2b481559701ad5730422f9b4474f7
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-01-19 12:14:42 -0300

    almost done

--- src/hamming/detect.py
+++ src/hamming/detect.py
@@ -1,4 +1,18 @@
 def hamming_detect(binary_string : str):
+    error_count = 0
+    binary_string = binary_string[4:]
+    binary_string_rev = list(binary_string)
+    binary_string_rev.reverse()
+    binary_string = ""
+    for b in binary_string_rev:
+        binary_string += b
+
+    print(binary_string)
+    #for i, ia in enumerate(binary_string):
+    #    if i % 4 == 0:
+    #        print(' ', end='')
+    #    print(ia, end='')
+    #print('\n')
     parity_bits = []
     general_parity_bit = binary_string[0]
     for i, bit in enumerate(binary_string):
@@ -7,8 +21,46 @@ def hamming_detect(binary_string : str):
     for i in range(len(parity_bits)):
         for j, bit in enumerate(binary_string):
             binary_position = bin(j).rjust(8, '0')
-            if binary_position[-i] == '1':
-                parity_bits[i][1].append(bit)
-    print(parity_bits)
-    
-        
+            if binary_position[-(i+1)] == '1':
+                parity_bits[i][1].append(j)
+    visual_parity_bits = parity_bits.copy()
+    #for e, i in enumerate(visual_parity_bits):
+    #    visual_parity_bits[e] = list(i)
+    #for e, i in enumerate(visual_parity_bits):
+    #    visual_parity_bits[e][1] = [binary_string[b] for b in i[1]]
+    #for i in visual_parity_bits:
+    #    print(i)
+    error_indexes = set({})
+    error_index = None
+    general_parity = False
+    one_count = 0
+    for i in binary_string:
+        if i == '1':
+            one_count += 1
+    if one_count % 2 == 0:
+        general_parity = True
+    for i in parity_bits:
+        parity_bit = i[0]
+        represented_bits = i[1]
+        one_count = 0
+        for represented_index in represented_bits:
+            if int(binary_string[represented_index]):
+                one_count += 1
+        if one_count % 2 != 0 and error_count == 0:
+            error_count += 1
+            if general_parity:
+                break
+            error_indexes.update(represented_bits)
+        elif one_count % 2 != 0 and error_count:
+
+            error_indexes.intersection_update(represented_bits)
+        print(error_indexes)
+    if general_parity and error_count:
+        error_count = 2
+    print(f"Erros achados: {error_count}")
+    for ia in error_indexes:
+        error_index = ia
+    if error_count == 1:
+        print(f"Posição (SEC): {error_index}\nBit: {binary_string[error_index]}")
+    elif error_count == 2:
+        print("Posição não pode ser identificada (DED).")
--- src/hamming/encode.py
+++ src/hamming/encode.py
@@ -1,13 +1,76 @@
-def hamming_encode(file_binary_string, parity_amount):
-    encoded_string = list(file_binary_string)
+import math
 
-    encoded_string.insert(0, "a")
+
+def hamming_encode(file_binary_string, parity_amount):
+    original_string = list(file_binary_string)
+    original_string_len_position = math.ceil(math.log2(len(original_string)))
+    original_string.reverse()
+    original_dictionary = {}
+    xors = []
 
     for i in range(parity_amount):
-        index = 2**(i)
+        for char_string_position in range(len(original_string)):
+            position_in_binary = str(bin(char_string_position))[2:].rjust(original_string_len_position + 1, '0')
+            original_dictionary[position_in_binary] = original_string[char_string_position]
+    print(original_dictionary)
+    
+    for i in range(parity_amount):
+        position = i
+        print(position)
+        xor = 0
+
+        keys = original_dictionary.keys()
+        for key in keys:
+            str_key = list(key)
+            str_key.reverse()
+            print(position, " => ", str_key, " => ", original_dictionary[key])
+            if(str_key[position] == '1'):
+                if(original_dictionary[key]) == '1':
+                    xor += 1
+        print("xor = ", xor)
+        xors.append(xor)
+
+    original_string.reverse()
+    for i in range(len(xors)):
+        position = 2 ** i
+        toBeInserted = 0
+        if(xors[i] % 2 != 0):
+            toBeInserted = 1
+        original_string.insert(position, str(toBeInserted))
+
+    original_string.reverse()
+    print(original_string)
+
+    sum = 0
+    for char in original_string:
+        sum += int(char)
+    
+    if(sum % 2 == 0):
+        original_string.insert(len(original_string) + 1, '0')
+    else:
+        original_string.insert(len(original_string) + 1, '1')
         
-        encoded_string.insert(index, "a")
+    print("xors = ", xors)
+    print(original_string)
+    print(original_dictionary)
 
-        parity_amount -= 1
+    char_string = ""
+    f = open("zoz_nice_dick.wham", "wb")
+    for char in original_string:
+        char_string += char
 
-    print(encoded_string)
+    print(char_string)
+
+    char1=int(char_string[0:8], 2)
+    char2=int(char_string[8:16], 2)
+
+    bin1 = bin(char1)
+    bin2 = bin(char2)
+
+    print(bin1, bin2)
+    print(char1, char2)
+
+    char_list = bytearray([char1, char2])
+    print(char_list)
+
+    f.write(char_list)
